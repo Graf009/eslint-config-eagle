febab8c921e9c34e4ca75c9de3f938e3f005952e
Oleg Orlov <mail@orlovoleg.com>
2016-04-14 14:15:51 +0300

    disable camelcase rule

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ module.exports = {
   'parser': 'babel-eslint',
   'extends': require.resolve('eslint-config-airbnb'),
   "rules": {
+    'camelcase': 0,
     'consistent-return': 0,
-    "comma-dangle": [ 2, "never" ],
+    'comma-dangle': [ 2, "never" ],
     'max-len': [2, 140, 2, { 'ignoreUrls': true, 'ignoreComments': false }],
-    "new-cap": 0,
-    "no-use-before-define": 0,
-    "no-param-reassign": [2, { 'props': false }],
-    "semi": [ 2, "never" ],
-    "valid-jsdoc": 2
+    'new-cap': 0,
+    'no-use-before-define': 0,
+    'no-param-reassign': [2, { 'props': false }],
+    'semi': [ 2, "never" ],
+    'valid-jsdoc': 2
   }
 };
