19959de04e77187c56d77664cd88287f795a7307
Oleg Orlov <mail@orlovoleg.com>
2016-04-15 13:18:36 +0300

    add rule eqeqeq

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ module.exports = {
     'camelcase': 0,
     'consistent-return': 0,
     'comma-dangle': [ 2, "never" ],
+    'eqeqeq': [2, 'allow-null'],
     'max-len': [2, 140, 2, { 'ignoreUrls': true, 'ignoreComments': false }],
     'new-cap': 0,
     'no-use-before-define': 0,
