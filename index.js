e117076990b42c78478f9f68ed6e650cc2e47581
Oleg Orlov <mail@orlovoleg.com>
2016-04-22 15:31:00 +0300

    disable rule no-underscore-dangle and global-require

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ module.exports = {
   'extends': require.resolve('eslint-config-airbnb'),
   "rules": {
     'camelcase': 0,
-    'consistent-return': 0,
     'comma-dangle': [ 2, "never" ],
+    'consistent-return': 0,
+    'global-require': 0,
     'max-len': [2, 140, 2, { 'ignoreUrls': true, 'ignoreComments': false }],
     'new-cap': 0,
-    'no-use-before-define': 0,
     'no-param-reassign': 0,
     'no-shadow': 0,
+    'no-underscore-dangle': 0,
+    'no-use-before-define': 0,
     'semi': [ 2, "never" ],
     'valid-jsdoc': 2
   }
