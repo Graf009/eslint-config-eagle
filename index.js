04660699ff42e75b65ee82952a944fe8cef1a963
Oleg Orlov <mail@orlovoleg.com>
2016-08-22 18:43:50 +0300

    add linting

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable quote-props */
 module.exports = {
   parser: require.resolve('babel-eslint'),
   parserOptions: {
@@ -6,26 +7,26 @@ module.exports = {
     ecmaFeatures: {
       jsx: true,
       generators: true,
-      experimentalObjectRestSpread: true
-    }
+      experimentalObjectRestSpread: true,
+    },
   },
 
   extends: require.resolve('eslint-config-airbnb'),
 
   rules: {
     'camelcase': 'off',
-    'comma-dangle': [ 'error', 'always-multiline' ],
+    'comma-dangle': ['error', 'always-multiline'],
     'consistent-return': 'off',
     'global-require': 'off',
     'import/no-extraneous-dependencies': 'off',
-    'max-len': ['error', 140, 2, { 'ignoreUrls': true, 'ignoreComments': false }],
+    'max-len': ['error', 140, 2, { ignoreUrls: true, ignoreComments: false }],
     'new-cap': 'off',
     'no-param-reassign': 'off',
     'no-shadow': 'off',
     'no-underscore-dangle': 'off',
     'no-use-before-define': 'off',
     'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
-    'semi': [ 'error', 'never' ],
+    'semi': ['error', 'never'],
     'valid-jsdoc': 'error',
-  }
-};
+  },
+}
